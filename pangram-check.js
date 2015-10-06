b71e71034c9dbf92c52cc3ad37aae23362527810
Roberto MF <git@virgulilla.com>
2015-10-07 01:23:22 +0200

    Switch to ES6 syntax

--- pangram-check.js
+++ pangram-check.js
@@ -1,39 +1,37 @@
-(function() {
-  var AlphabetInput = React.createClass({
-    render: function() {
-      return (
-        <div className="alphabet-input">
-          <label for="alphabet">Alphabet: </label>
-          <input type="text" id="alphabet" value="abcdefghijklmnñoprstuvwxyz" />
-        </div>
-      );
-    }
-  });
+class AlphabetInput extends React.Component {
+  render() {
+    return (
+      <div className="alphabet-input">
+        <label for="alphabet">Alphabet: </label>
+        <input type="text" id="alphabet" value="abcdefghijklmnñoprstuvwxyz" />
+      </div>
+    );
+  }
+}
 
-  var PangramInput = React.createClass({
-    render: function() {
-      return (
-        <div className="pangram-input">
-          <label for="pangram">Pangram: </label>
-          <input type="text" id="pangram" value="El husky vietnamita excava en busca de galletas. Su quijotesco dueño sube una foto a la web con presteza." />
-        </div>
-      );
-    }
-  });
+class PangramInput extends React.Component {
+  render() {
+    return (
+      <div className="pangram-input">
+        <label for="pangram">Pangram: </label>
+        <input type="text" id="pangram" value="El husky vietnamita excava en busca de galletas. Su quijotesco dueño sube una foto a la web con presteza." />
+      </div>
+    );
+  }
+}
 
-  var PangramChecker = React.createClass({
-    render: function() {
-      return (
-        <div className="pangram-checker">
-          <AlphabetInput />
-          <PangramInput />
-        </div>
-      );
-    }
-  });
+class PangramChecker extends React.Component {
+  render() {
+    return (
+      <div className="pangram-checker">
+        <AlphabetInput />
+        <PangramInput />
+      </div>
+    );
+  }
+}
 
-  React.render(
-    <PangramChecker />,
-    document.getElementById('pangram-checker')
-  );
-}());
+React.render(
+  <PangramChecker />,
+  document.getElementById('pangram-checker')
+);
