d97d64421a3054dd1d680f592a51eb0f488852f6
Roberto MF <roboe@virgulilla.com>
2017-03-22 16:33:22 +0100

    Make PangramInput.js work

--- src/components/PangramInput.js
+++ src/components/PangramInput.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 
 class PangramInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  prepareSentence(sentence) {
+    return sentence.trim().toLowerCase();
+  }
+
+  handleChange(event) {
+    this.props.updateSentence(this.prepareSentence(event.target.value));
+  }
+
   render() {
     return (
       <div className="input-container">
         <label htmlFor="pangram-input">Pangram</label>
-        <input type="text" id="pangram-input" defaultValue={this.props.sentence}
-          onChange={console.log.bind(console)} />
+        <input type="text" id="pangram-input"
+          defaultValue={this.props.sentence}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
