ec27b480a9ee767696f43528711adaa87f7ab937
Roberto MF <git@virgulilla.com>
2020-03-12 22:22:28 +0100

    Add a pipe() function

--- src/application/helpers/functional.js
+++ src/application/helpers/functional.js
@@ -1,3 +1,5 @@
 const applyFunc = (f, g) => (value) => g(f(value))
 
 export const compose = (...functions) => functions.reduceRight(applyFunc);
+
+export const pipe = (...functions) => functions.reduce(applyFunc);
