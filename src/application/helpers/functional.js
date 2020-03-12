21648d56bbf59aa4c69d8e778d5a5e1185448427
Roberto MF <git@virgulilla.com>
2020-03-12 22:21:46 +0100

    Refactor compose()

--- src/application/helpers/functional.js
+++ src/application/helpers/functional.js
@@ -1,5 +1,3 @@
-export const compose = (...functions) => functions
-  .reduceRight(
-    (prevFn, currentFn) => value => currentFn(prevFn(value)),
-    value => value
-  );
+const applyFunc = (f, g) => (value) => g(f(value))
+
+export const compose = (...functions) => functions.reduceRight(applyFunc);
