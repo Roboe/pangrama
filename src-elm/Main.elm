5c408296fc1cee688e4a1f741af1390ec7fb3410
Roberto MF <git@virgulilla.com>
2020-04-29 20:06:35 +0200

    Import specific Html.Events

--- src-elm/Main.elm
+++ src-elm/Main.elm
@@ -4,7 +4,7 @@ module Main exposing (main)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (..)
+import Html.Events exposing (onInput)
 import Pangram
 import Utils.Html exposing (defaultValue, role, ariaLabel)
 
