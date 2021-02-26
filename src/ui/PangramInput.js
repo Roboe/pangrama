e15018af1d5e4f97b9e14b6e10e0798e4cfeb142
Roberto MF <git@virgulilla.com>
2021-02-26 23:11:00 +0100

    Style: format PangramInput.js

--- src/ui/PangramInput.js
+++ src/ui/PangramInput.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react';
-import { compose } from '../application/helpers/functional';
-import { prepareForShowing as prepareSentence } from '../application/pangram';
-
+import React, { Component } from 'react'
+import { compose } from '../application/helpers/functional'
+import { prepareForShowing as prepareSentence } from '../application/pangram'
 
 class PangramInput extends Component {
   handleChange = compose(
     this.props.updateSentence,
     prepareSentence,
-    (event) => event.target.value,
+    (event) => event.target.value
   )
 
   render = () => (
     <div className="pc-text-field">
-      <input type="text" id="pangram-input" className="pc-text-field--input"
-        defaultValue={ this.props.sentence }
-        onChange={ this.handleChange }
+      <input
+        type="text"
+        id="pangram-input"
+        className="pc-text-field--input"
+        defaultValue={this.props.sentence}
+        onChange={this.handleChange}
       />
-      <label htmlFor="pangram-input" className="pc-text-field--label">Pangram</label>
+      <label htmlFor="pangram-input" className="pc-text-field--label">
+        Pangram
+      </label>
     </div>
   )
 }
 
-export default PangramInput;
+export default PangramInput
