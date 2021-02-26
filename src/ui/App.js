49dfffd9971b8ead51c73a468622d1ccd4ca615b
Roberto MF <git@virgulilla.com>
2021-02-26 22:47:34 +0100

    Refactor: extract UI components from App and use composition

--- src/ui/App.js
+++ src/ui/App.js
@@ -5,6 +5,49 @@ import Validator from './Validator';
 import NewTabLink from './presentational/NewTabLink';
 import Emoji from './presentational/Emoji';
 
+const UIHeader = () => (
+  <header className="pc-header">
+    <span className="pc-header--title">Pangram Checker</span>
+  </header>
+);
+
+const UIMain = ({ alphabet, sentence, updateAlphabet, updateSentence }) => (
+  <main className="pc-main">
+    <section className="pc-main--section">
+      <AlphabetInput alphabet={alphabet} updateAlphabet={updateAlphabet} />
+      <PangramInput sentence={sentence} updateSentence={updateSentence} />
+    </section>
+    <section className="pc-main--section">
+      <Validator alphabet={alphabet} sentence={sentence} />
+    </section>
+  </main>
+);
+
+const UIFooter = () => (
+  <footer className="pc-footer">
+    <span className="pc-footer--credits">
+      Made by <NewTabLink url="http://virgulilla.com">@RoboePi</NewTabLink>{" "}
+      <Emoji emoji="&#x1f989;" label="Owl" />
+    </span>
+    <span className="pc-footer--license">
+      <NewTabLink url="https://github.com/Roboe/pangrama">Source</NewTabLink>{" "}
+      (GPLv3)
+    </span>
+  </footer>
+);
+
+const UIApp = ({ header, main, footer }) => {
+  return (
+    <div id="pangram-checker" className="pc-structure">
+      <div className="pc-structure--header">{header}</div>
+      <div className="pc-structure--body">
+        {main}
+        {footer}
+      </div>
+    </div>
+  );
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -34,41 +77,18 @@ class App extends Component {
     const { alphabet, sentence } = this.state;
 
     return (
-      <div id="pangram-checker" className="pc-structure">
-        <div className="pc-structure--header">
-          <header className="pc-header">
-            <span className="pc-header--title">Pangram Checker</span>
-          </header>
-        </div>
-        <div className="pc-structure--body">
-          <main className="pc-main">
-            <section className="pc-main--section">
-              <AlphabetInput
-                alphabet={ alphabet }
-                updateAlphabet={ this.updateAlphabet }
-              />
-              <PangramInput
-                sentence={ sentence }
-                updateSentence={ this.updateSentence }
-              />
-            </section>
-            <section className="pc-main--section">
-              <Validator
-                alphabet={ alphabet }
-                sentence={ sentence }
-              />
-            </section>
-          </main>
-          <footer className="pc-footer">
-            <span className="pc-footer--credits">
-              Made by <NewTabLink url="http://virgulilla.com">@RoboePi</NewTabLink> <Emoji emoji="&#x1f989;" label="Owl" />
-            </span>
-            <span className="pc-footer--license">
-              <NewTabLink url="https://github.com/Roboe/pangrama">Source</NewTabLink> (GPLv3)
-            </span>
-          </footer>
-        </div>
-      </div>
+      <UIApp
+        header={<UIHeader />}
+        main={
+          <UIMain
+            alphabet={alphabet}
+            sentence={sentence}
+            updateAlphabet={this.updateAlphabet}
+            updateSentence={this.updateSentence}
+          />
+        }
+        footer={<UIFooter />}
+      />
     );
   }
 }
