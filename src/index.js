1a005dd36b5e4a07781f628c474428735113d718
Roberto MF <git@virgulilla.com>
2021-03-27 00:55:02 +0100

    Fix: disable malfunctioning Service Worker (closes #1)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom'
 
-import registerServiceWorker from './infrastructure/registerServiceWorker'
+import unregister from './infrastructure/registerServiceWorker'
 import App from './application/App'
 
 import './main.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+
+// Disable malfunctioning SW for users who have already registered it
+unregister()
