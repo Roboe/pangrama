Roboe/pangrama

commit e2b6fd11872c6accfd0b01a48ac9eaf065587b44

Refactor: colocate application components

src/App/PangramInput.jsx
@@ -2,11 +2,9 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import TextField from '../ui/TextField'
-
-import { pipe } from './helpers/functional'
-
-import { prepareForShowing as prepareSentence } from './pangram'
+import { pipe } from '../libs/functional'
+import { prepareForShowing as prepareSentence } from '../core/pangram'
+import LabeledTextField from '../uikit/LabeledTextField'
 
 const PangramInput = ({ sentence, updateSentence }) => {
   const handleChange = pipe(
@@ -16,13 +14,15 @@ const PangramInput = ({ sentence, updateSentence }) => {
   )
 
   return (
-    <TextField
+    <LabeledTextField
       id="pangram-input"
       label="Pangrama"
       isMultiline
       rows={3}
-      defaultValue={sentence}
-      handleChange={handleChange}
+      inputProps={{
+        defaultValue: sentence,
+        onChange: handleChange,
+      }}
     />
   )
 }

src/App/AlphabetInput.jsx
@@ -2,11 +2,9 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import TextField from '../ui/TextField'
-
-import { pipe } from './helpers/functional'
-
-import { prepare as prepareAlphabet } from './alphabet'
+import { prepare as prepareAlphabet } from '../core/alphabet'
+import { pipe } from '../libs/functional'
+import LabeledTextField from '../uikit/LabeledTextField'
 
 const AlphabetInput = ({ alphabet, updateAlphabet }) => {
   const handleChange = pipe(
@@ -16,11 +14,13 @@ const AlphabetInput = ({ alphabet, updateAlphabet }) => {
   )
 
   return (
-    <TextField
-      id="alphabet-input"
+    <LabeledTextField
       label="Alfabeto"
-      defaultValue={alphabet}
-      handleChange={handleChange}
+      inputProps={{
+        id: 'alphabet-input',
+        defaultValue: alphabet,
+        onChange: handleChange,
+      }}
     />
   )
 }

src/App/Share.jsx
@@ -2,9 +2,8 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import UrlField from '../ui/UrlField'
-
-import { writeSearchParamsToUrl } from './searchParams'
+import { writeSearchParamsToUrl } from '../browser/searchParams'
+import UrlTextField from '../uikit/UrlTextField'
 
 const copyTextToClipboard = (text) => {
   if (!navigator.clipboard) return false
@@ -23,7 +22,7 @@ const Share = ({ alphabet, sentence, children }) => {
   }
 
   return (
-    <UrlField
+    <UrlTextField
       value={url.href}
       handleClick={handleCopyButton}
       buttonChildren={children}

src/App/Structure/Footer/index.jsx
@@ -1,0 +1,14 @@
+// SPDX-FileCopyrightText: 2021 Roberto MF (Roboe)
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import './style.css'
+
+const Footer = ({ credits, license }) => (
+  <footer className="pc-footer">
+    <span className="pc-footer--credits">{credits}</span>
+    <span className="pc-footer--license">{license}</span>
+  </footer>
+)
+
+export default Footer

src/App/Structure/Header/index.jsx
@@ -1,0 +1,15 @@
+// SPDX-FileCopyrightText: 2021 Roberto MF (Roboe)
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import './style.css'
+
+const Header = ({ title, link }) => (
+  <header className="pc-header">
+    <a className="pc-header--title" href={link}>
+      {title}
+    </a>
+  </header>
+)
+
+export default Header

src/App/Structure/Main/index.jsx
@@ -1,0 +1,20 @@
+// SPDX-FileCopyrightText: 2021 Roberto MF (Roboe)
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import './style.css'
+
+const Main = ({ intro, sections }) => (
+  <main className="pc-main">
+    <section className="pc-main--section pc-main--section_intro">
+      {intro}
+    </section>
+    {sections.map((section, index) => (
+      <section key={index} className="pc-main--section">
+        {section}
+      </section>
+    ))}
+  </main>
+)
+
+export default Main

commit 62c5a73d7a7b55bc6b68ae5aa446a6649177a2d6

Fix(PangramInput): pass input props correctly
This issue was introduced in e2b6fd11

src/App/PangramInput.jsx
@@ -15,13 +15,13 @@ const PangramInput = ({ sentence, updateSentence }) => {
 
   return (
     <LabeledTextField
-      id="pangram-input"
       label="Pangrama"
       isMultiline
-      rows={3}
       inputProps={{
+        id: 'pangram-input',
         defaultValue: sentence,
         onChange: handleChange,
+        rows: 3,
       }}
     />
   )